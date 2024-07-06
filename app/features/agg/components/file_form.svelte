c1b388bb45000aa0675ad5687fe0ddcd52604e14
uranum12 <113694643+uranum12@users.noreply.github.com>
2024-07-06 18:38:03 +0900

    remove files selector component

--- app/features/agg/components/file_form.svelte
+++ app/features/agg/components/file_form.svelte
@@ -7,9 +7,11 @@
 </script>
 
 <script lang="ts">
-  import ConfirmDialog from "@/components/confirm_dialog.svelte"
+  import { untrack } from "svelte"
 
-  import FilesSelector from "./files_selector.svelte"
+  import Accordion from "@/components/accordion.svelte"
+  import Alert from "@/components/alert.svelte"
+  import ConfirmDialog from "@/components/confirm_dialog.svelte"
 
   type Props = {
     files: string[]
@@ -22,11 +24,43 @@
   let filename = $state<string>("")
   let overwrite = $state<boolean>(false)
 
+  let filtered = $state<string[]>(files.sort())
+  let checked = $state<string[]>([])
+  let filter = $state<string>("")
+
+  const hidden = $derived(files.filter((file) => !filtered.includes(file)))
+
+  $effect(() => {
+    selected = untrack<string[]>(() => selected)
+      .filter((file) => hidden.includes(file))
+      .concat(checked)
+  })
+
   let showConfirmOverwrite = $state<boolean>(false)
 
-  const submitDisabled = $derived<boolean>(
-    selected.length === 0 || filename.trim() === ""
-  )
+  const fileSelectInvalid = $derived(selected.length === 0)
+
+  const submitDisabled = $derived(fileSelectInvalid || filename.trim() === "")
+
+  const filterApply = () => {
+    filtered = files.filter((file) =>
+      file.replace(/^data\//, "").includes(filter)
+    )
+    checked = selected.filter((file) => filtered.includes(file))
+  }
+
+  const filterClear = () => {
+    filter = ""
+    filterApply()
+  }
+
+  const selectAll = () => {
+    checked = filtered
+  }
+
+  const deselectAll = () => {
+    checked = []
+  }
 
   const submit = () => {
     onSubmit({ files: selected, filename, overwrite })
@@ -46,9 +80,37 @@
 </script>
 
 <section>
-  <FilesSelector {files} bind:selected />
+  <Accordion class="mb-1" summary="filter">
+    <input bind:value={filter} class="mb-1" />
+    <button onclick={filterApply}>apply</button>
+    <button onclick={filterClear}>clear</button>
+  </Accordion>
+
+  {#if filtered.length === 0}
+    <Alert type="warning">
+      <p>no files matched</p>
+    </Alert>
+  {:else}
+    <div class="mb-1">
+      <button onclick={selectAll}>select all files</button>
+      <button onclick={deselectAll}>deselect all files</button>
+    </div>
+
+    <div
+      class="mb-1 max-h-96 overflow-y-auto rounded border border-gray-300 p-2"
+      class:!border-red-300={fileSelectInvalid}
+    >
+      {#each filtered as file}
+        <label>
+          <input type="checkbox" bind:group={checked} value={file} />
+          <span class="ml-1">{file.replace(/^data\//, "")}</span>
+        </label>
+      {/each}
+    </div>
+  {/if}
+
   <input
-    class="my-1"
+    class="mb-1"
     placeholder="output file name"
     required
     bind:value={filename}
